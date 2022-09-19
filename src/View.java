9d1a50af810a427b0353f7b69886e42d73037a0f
Tini <tini@MacBook-Pro-de-Tini.local>
2022-09-19 16:08:45 +0200

    resized Window and etc.

--- src/View.java
+++ src/View.java
@@ -1,17 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Graphics;
 
 public class View extends JFrame {
-    JPanel main;
-    // Model model = new Model();
-    // Controller controller = new Controller(this, model);
+    JPanel main = new JPanel();
+    Model model = new Model(Color.BLACK);
+    Controller controller = new Controller(this, model);
 
     public View() {
-        this.setSize(500,500);
+        this.setSize(750,750);
         this.setResizable(false);
         this.setTitle("Paint_Me");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.getContentPane().add(main);
         this.setVisible(true);
     }
 }
